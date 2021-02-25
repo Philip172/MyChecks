fd08d4462a9aa61a387538650a1e2561d4959365
fill shmykov <fshmykov@gmail.com>
2021-02-25 14:33:05 +0400

    Add project files.

---
+++ CheckBigInteger/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace CheckBigInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var save = false;
+            BigInteger big = 2;
+
+            while (!save)
+            {
+                try
+                {
+                    big *= big;
+
+                    Console.WriteLine("\n\n\n\n");
+                    Console.WriteLine(big);
+                }
+                catch (OutOfMemoryException)
+                {
+                    Console.WriteLine("ERROR MEMORY");
+                    save = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                    save = true;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
